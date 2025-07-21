116cb21b595642b118f5bdd3aa24e4beb72c8ac2
Bond52 <bertrand.ond@gmail.com>
2025-07-21 16:59:37 -0400

    Ajout du code dans les pages de login

---
+++ app/acheteur/page.tsx
@@ -0,0 +1,8 @@
+export default function AcheteurPage() {
+  return (
+    <div>
+      <h1>Bienvenue acheteur</h1>
+      <p>Vous êtes maintenant connecté en tant qu'acheteur.</p>
+    </div>
+  );
+}
---
+++ app/vendeur/page.tsx
@@ -0,0 +1,8 @@
+export default function VendeurPage() {
+  return (
+    <div>
+      <h1>Bienvenue vendeur</h1>
+      <p>Vous êtes maintenant connecté en tant que vendeur.</p>
+    </div>
+  );
+}
