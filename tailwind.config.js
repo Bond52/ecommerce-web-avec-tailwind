21fbe14ec5210a7dec5c97859fc0c4801230bfb1
Bertrand Ond <bertrand.ond@email.com>
2025-09-11 20:18:37 -0400

    Fix Tailwind config paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
-  "./src/app/**/*.{js,ts,jsx,tsx}",
-  "./src/components/**/*.{js,ts,jsx,tsx}"
-],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
