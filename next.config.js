da34fea0350d1947d24ccfd2feab545bcdacbd26
Bertrand Ond <bertrand.ond@email.com>
2025-10-06 18:34:37 -0400

    rollback next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // ✅ Autorise les images hébergées sur ton backend Render
-  images: {
-    domains: ["ecommerce-web-avec-tailwind.onrender.com"],
-  },
-
-  // ✅ Variable d’environnement publique accessible côté client
-  env: {
-    NEXT_PUBLIC_BACKEND_URL:
-      process.env.NEXT_PUBLIC_BACKEND_URL ||
-      "https://ecommerce-web-avec-tailwind.onrender.com",
-  },
-
-  // 🚫 Supprime complètement rewrites()
-  // car Next 13.4.x ne permet pas l’usage de process.env ici au build
 };
 
 module.exports = nextConfig;
