670e5988779adb9711644438d78cdd7a09a9d930
Bertrand Ond <bertrand.ond@email.com>
2025-10-16 12:53:40 -0400

    page artisans v2

--- backend-api/routes/artisans.js
+++ backend-api/routes/artisans.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User");
+const User = require("../models/user");
 
 // GET /api/artisans → liste des vendeurs
 router.get("/", async (req, res) => {
